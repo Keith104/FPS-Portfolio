1c25b6dc167d0302fe248553f48060a408d32674
KeithJanish <keithjanish18@gmail.com>
2025-07-07 14:52:00 -0400

    AHHHH

--- FPS-Portfolio/Assets/Scripts/ZoomIn.cs
+++ FPS-Portfolio/Assets/Scripts/ZoomIn.cs
@@ -6,6 +6,9 @@ public class ZoomIn : MonoBehaviour
 
      [SerializeField] int inFOV;  // the fully zoomed in zoom level
      [SerializeField] int outFOV; // regular zoom level
+    [SerializeField] int lerpSpeed;
+
+    bool isZooming;
 
      // Start is called once before the first execution of Update after the MonoBehaviour is created
      void Start()
@@ -16,18 +19,16 @@ public class ZoomIn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-          Zoom();
+        Zoom();
     }
-     void Zoom() // this does a Perspective Zoom
-     {
-          // GetMouseButtonDown(1) checks if the right mouse button is pressed
-          if (Input.GetMouseButtonDown(1))
-          {
-               playerCam.fieldOfView = inFOV;
-          }
-          else if (Input.GetMouseButtonUp(1))
-          {
-               playerCam.fieldOfView = outFOV;
-          }
-     }
+
+    void Zoom()
+    {
+        if (Input.GetMouseButtonDown(1)) isZooming = true;
+        else if (Input.GetMouseButtonUp(1)) isZooming = false;
+
+        float target = isZooming ? inFOV : outFOV;
+
+        playerCam.fieldOfView = Mathf.Lerp(playerCam.fieldOfView, target, lerpSpeed * Time.deltaTime);
+    }
 }
