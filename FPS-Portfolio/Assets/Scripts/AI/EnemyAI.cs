9bd38d394dfb0c7c84f01f87176a14d58890cd46
Erick Marin <erick.j1025@gmail.com>
2025-07-23 18:01:58 -0400

    Fixed AI
    Updated AI Death animations, Audio for AI

--- FPS-Portfolio/Assets/Scripts/AI/EnemyAI.cs
+++ FPS-Portfolio/Assets/Scripts/AI/EnemyAI.cs
@@ -23,6 +23,19 @@ public class EnemyAI : MonoBehaviour, IDamage
 
     protected GameObject player;
 
+
+
+    [SerializeField] AudioSource audio;
+    [SerializeField] AudioClip[] audioHurt;
+    [SerializeField] float audioHurtVol;
+    [SerializeField] AudioClip[] audioStep;
+    [SerializeField] float audioStepVol;
+    [SerializeField] AudioClip[] audioShoot;
+    [SerializeField] float audioShootVol;
+
+    bool isSprinting;
+    bool isPlayingStep;
+
     Color colorOg;
 
     float searchTime;
@@ -94,6 +107,8 @@ public class EnemyAI : MonoBehaviour, IDamage
 
         StartCoroutine(FlashRed());
 
+        audio.PlayOneShot(audioHurt[Random.Range(0, audioHurt.Length)], audioHurtVol);
+
         if (health <= 0)
         {
             if (!tutorial)
@@ -102,14 +117,22 @@ public class EnemyAI : MonoBehaviour, IDamage
             }
             GameManager.instance.UpdateTotalScoreText(amountToScore);
             GameManager.instance.UpdateWaveScoreText(amountToScore);
-            Destroy(gameObject);
+            StartCoroutine(DestroyAfterDelay());
         }
+    }
+
+    IEnumerator DestroyAfterDelay()
+    {
+        agent.isStopped = true;
+        animate.SetTrigger("Dead");
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
     }
 
     IEnumerator FlashRed()
     {
         model.material.color = Color.red;
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(0.3f);
         model.material.color = colorOg;
     }
     IEnumerator stunTime(float stunTime)
@@ -138,7 +161,25 @@ public class EnemyAI : MonoBehaviour, IDamage
             NavMeshHit hit;
             NavMesh.SamplePosition(ranPos, out hit, searchDist, 1);
             agent.SetDestination(hit.position);
+            if (!isPlayingStep)
+            {
+                StartCoroutine(playStep());
+            }
         }
+    }
+
+    IEnumerator playStep()
+    {
+        isPlayingStep = true;
+        audio.PlayOneShot(audioStep[Random.Range(0, audioStep.Length)], audioStepVol);
+
+        if (isSprinting)
+        {
+            yield return new WaitForSeconds(0.3f);
+        }
+        else
+            yield return new WaitForSeconds(0.5f);
+        isPlayingStep = false;
     }
 
     public virtual bool CanSeePlayer()
@@ -205,6 +246,8 @@ public class EnemyAI : MonoBehaviour, IDamage
         shootTimer = 0;
 
         animate.SetTrigger("Shoot");
+
+        audio.PlayOneShot(audioShoot[Random.Range(0, audioShoot.Length)], audioShootVol);
 
         Instantiate(bullet, shootPos.position, Quaternion.LookRotation(playerDir));
     }
